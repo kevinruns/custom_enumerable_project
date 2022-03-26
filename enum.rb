9d0618301a0e036d953b95d8c00e5c4a0e488407
Kevin Lenehan <klenehan123@gmail.com>
2022-03-26 10:06:24 +0000

    all tests working

---
+++ enum.rb
@@ -0,0 +1,6 @@
+require_relative './lib/my_enumerables.rb'
+
+puts "my_each vs. each"
+numbers = [1, 2, 3, 4, 5]
+numbers.my_each  { |item| puts item }
+numbers.each  { |item| puts item }
--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,81 @@
 module Enumerable
   # Your code goes here
+
+  def my_count
+    i = 0
+    for item in self
+      unless block_given? && !yield(item)
+          i = i + 1
+      end
+    end
+    return i
+  end
+
+  def my_any?
+    bool = false
+    for item in self
+      if yield(item)
+        bool = true
+      end
+    end
+    return bool
+  end
+
+  def my_each_with_index
+    i = 0
+    for item in self
+      yield(item, i)
+      i = i + 1
+    end
+  end
+
+  def my_inject(start)
+    total = start
+    for item in self
+      total = yield(total, item)
+    end  
+    return total
+  end
+
+  def my_none?
+    bool = true
+    for item in self
+      if yield(item)
+        bool = false
+      end
+    end
+    return bool
+  end
+
+  def my_all?
+    bool = true
+    for item in self
+      unless yield(item)
+        bool = false
+      end
+    end
+    return bool
+  end
+
+
+  def my_map
+    total = []
+    for item in self
+      total << yield(item)
+    end  
+    return total
+  end
+
+  def my_select
+    arr = []
+    for item in self
+      if (yield(item))
+        arr << item
+      end
+    end  
+    return arr
+  end
+
 end
 
 # You will first have to define my_each
@@ -8,4 +84,11 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    for item in self
+      yield(item)
+    end
+  end
+
 end
+
